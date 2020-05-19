8f33ba9240fd9f61aa9b96cc93e8d4f7eb4e6f46
Cavcaliuc <cavcaliucnina09@gmail.com>
2020-05-20 02:16:15 +0300

    some changes

--- app/src/main/java/com/example/myapplication/user/viewmodel/UserMainViewModel.kt
+++ app/src/main/java/com/example/myapplication/user/viewmodel/UserMainViewModel.kt
@@ -2,7 +2,11 @@ package com.example.myapplication.user.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myapplication.model.data.PlateData
+import com.example.myapplication.model.remote.ApiImpl
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UserMainViewModel : ViewModel() {
     val platesTokDetails = MutableLiveData<PlateData>()
@@ -10,7 +14,7 @@ class UserMainViewModel : ViewModel() {
     fun login(email: String, password: String) {
         viewModelScope.launch(Dispatchers.Default) {
             kotlin.runCatching {
-                ApiImpl().login(json)
+                ApiImpl().login(token)
             }.onSuccess {
                 if(it.success){
                     loginDetails.postValue(it.data)
